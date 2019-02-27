09fd1a0e69eaa61102423a98c4205e3abafb68c8
sohansarma <sohansarmamazumder_s@srmuniv.edu.in>
2019-02-27 23:46:36 +0530

    Project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import App from './containers/App';
+
+
+ReactDOM.render(<App /> ,document.getElementById('root'));
+
+
+serviceWorker.unregister();
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Aaditya Narayan Mishra',
+	mail: 'aaditya@gmail.com',
+	branch: 'Computer Science Engineering'
+},
+{
+	id: 2,
+	name: 'Gaurav Khatri',
+	mail: 'khatrigaurav@gmail.com',
+	branch: 'Information Technology'
+},
+{
+	id: 3,
+	name: 'Sreyansh Parihar',
+	mail: 'sreyansh@gmail.com',
+	branch: 'Information Technology'
+},
+{
+	id: 4,
+	name: 'Lokesh Raj Arora',
+	mail: 'aroraraj@gmail.com',
+	branch: 'Computer Science Engineering'
+},
+{
+	id: 5,
+	name: 'Sohan Sarma Mazumder',
+	mail: 'sohansarma@gmail.com',
+	branch: 'Software Engineering'
+},
+{
+	id: 6,
+	name: 'Jayesh Parihar',
+	mail: 'pariharjayesh@gmail.com',
+	branch: 'Automobile Engineering'
+},
+{
+	id: 7,
+	name: 'Siddharth Jha',
+	mail: 'jhasidd@gmail.com',
+	branch: 'Bio-technology Engineering'
+},
+{
+	id: 8,
+	name: 'Ankit Prasad',
+	mail: 'ankitprasad@gmail.com',
+	branch: 'Information Technology'
+},
+{
+	id: 9,
+	name: 'Swarnavya Sarkar',
+	mail: 'sarkar@gmail.com',
+	branch: 'Software Engineering'
+},
+{
+	id: 10,
+	name: 'Abhay Singh',
+	mail: 'abhaysingh@gmail.com',
+	branch: 'Software Engineering'
+},
+];
